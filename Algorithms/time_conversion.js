276b8ef48a2433bc05adfb99d9b9c9b51ec54d31
Reinz_IWR <71551362+irfanwahyr@users.noreply.github.com>
2025-05-24 12:59:01 +0700

    Update time_conversion.js

--- Algorithms/time_conversion.js
+++ Algorithms/time_conversion.js
@@ -1,1 +1,20 @@
+function timeConversion(s) {
+    let a = s.replace("AM", "").replace("PM", "");
+    a = s.split(":");
+    
+    
+    if(a[2].includes("PM")){
+        a[2] = a[2].replace("PM", "");
+        if(parseInt(a[0]) == 12) return a.join(":")
+        
+        a[0] = parseInt(a[0]) + 12;
+        return a.join(":");
+    } else {
+        if(parseInt(a[0]) > 11) a[0] = "00";
+        a[2] = a[2].replace("AM", "");
+        return a.join(":");
+    }
+}
 
+
+timeConversion("12:05:45PM")
